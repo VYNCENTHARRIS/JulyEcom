c6d997293c3c12de8ed9308aa1ce4597e31702d0
Vyncent Harris <vyncentharris@AdminisatorsMBP.attlocal.net>
2024-07-22 14:23:59 -0400

    Update server configuration to use environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 // Import modules
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -7,7 +8,7 @@ const path = require("path");
 const bcrypt = require("bcrypt"); // Will encrypt the password
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -16,8 +17,6 @@ app.use(bodyParser.json());
 app.use("/public", express.static(path.join(__dirname, "../public")));
 
 // Create a connection to the database
-require("dotenv").config();
-
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
