ce567315d496a0e3a622cb24c6f3d0b4cc092b2f
Vyncent Harris <vyncentharris@AdminisatorsMBP.attlocal.net>
2024-07-26 13:24:43 -0400

    Update server configuration to serve static files from build directory

--- backend/server.js
+++ backend/server.js
@@ -208,7 +208,7 @@ app.post("/contact", (req, res) => {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build", "index.html"));
+  res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
 app.listen(port, () => {
